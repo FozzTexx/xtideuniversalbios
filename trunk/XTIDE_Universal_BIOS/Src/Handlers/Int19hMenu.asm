5b9b55cb2ba377d1ae4a3f3ad4137b697f154683
gregli@hotmail.com <gregli@hotmail.com@143130fb-dee8-9d2b-0d25-209ee59ef9c5>
2012-01-16 20:50:39 +0000

    Fixed a bug where DL was always zero when transitioning control to the MBR (boot sector), where it should have been the drive number.  This was preventing FreeDOS from booting.  Also took the opportunity to fold two code paths together, jump to boot sector and jump to rom boot, resulting in a savings of 20 bytes for the XT build.

--- trunk/XTIDE_Universal_BIOS/Src/Handlers/Int19hMenu.asm
+++ trunk/XTIDE_Universal_BIOS/Src/Handlers/Int19hMenu.asm
@@ -54,52 +54,63 @@ ALIGN JUMP_ALIGN
 	call	DriveXlate_ToOrBack							; Translate drive number
 	call	BootSector_TryToLoadFromDriveDL
 	jnc		SHORT .ProcessMenuSelectionsUntilBootable	; Boot failure, show menu again
-	SWITCH_BACK_TO_POST_STACK
-	; Fall to JumpToBootSector
+	; Fall to Int19hMenu_JumpToBootSector_or_RomBoot
+	; (CF is set or we wouldn't be here, see "jnc" immediately above)
 
 ;--------------------------------------------------------------------
-; JumpToBootSector
+; Int19hMenu_JumpToBootSector_or_RomBoot
+;
+; Switches back to the POST stack, clears the DS and ES registers,
+; and either jumps to the MBR (Master Boot Record) that was just read,
+; or calls the ROM's boot routine on interrupt 18.
+;
 ;	Parameters:
 ;		DL:		Drive to boot from (translated, 00h or 80h)
-;		ES:BX:	Ptr to boot sector
+;       CF:     Set for Boot Sector Boot 
+;               Clear for Rom Boot
+;	   	ES:BX:	(if CF set) Ptr to boot sector
+;
 ;	Returns:
 ;		Never returns
 ;--------------------------------------------------------------------
 ALIGN JUMP_ALIGN
-JumpToBootSector:
-	push	es								; Push boot sector segment
-	push	bx								; Push boot sector offset
-	call	ClearSegmentsForBoot
-	retf
+Int19hMenu_JumpToBootSector_or_RomBoot:
+		mov		cx,es		; preserve MBR segment
+;
+; inline of SWITCH_BACK_TO_POST_STACK macro, so that we can preserve CF
+; and reuse zero value in ax for clearing segment registers
+;
+		mov		ax,0		; NOTE: can't use XOR as it impacts CF
+%ifndef USE_386
+		cli
+		mov		ss,ax
+		mov		sp,[ss:BOOTVARS.dwPostStack]
+		mov		ss,[ss:BOOTVARS.dwPostStack+2]
+		sti
+%else
+		mov		ss,ax
+		lss		sp,[ss:BOOTVARS.dwPostStack]
+%endif	
+;
+; end inline of SWITCH_BACK_TO_POST_STACK
+;
 
+; clear segment registers before boot sector or rom call
+; (old ClearSegmentsForBoot routine)
+;
+		mov		ds,ax		
+		mov		es,ax
 
-;--------------------------------------------------------------------
-; Int19hMenu_RomBoot
-;	Parameters:
-;		DS:		RAMVARS segment
-;	Returns:
-;		Never returns
-;--------------------------------------------------------------------
-ALIGN JUMP_ALIGN
-Int19hMenu_RomBoot:
-	SWITCH_BACK_TO_POST_STACK
-	call	ClearSegmentsForBoot
-	int		BIOS_BOOT_FAILURE_INTERRUPT_18h	; Never returns
+		jnc		.romboot
 
+; jump to boot sector
+; (old JumpToBootSector routine)
+;
+		push	cx			; sgment address for MBR
+		push	bx			; offset address for MBR
+		retf				; NOTE:	DL is set to the drive number
 
-;--------------------------------------------------------------------
-; ClearSegmentsForBoot
-;	Parameters:
-;		Nothing
-;	Returns:
-;		DX:		Zero
-;		DS=ES:	Zero
-;	Corrupts registers:
-;		Nothing
-;--------------------------------------------------------------------
-ALIGN JUMP_ALIGN
-ClearSegmentsForBoot:
-	xor		dx, dx					; Device supported by INT 13h
-	mov		ds, dx
-	mov		es, dx
-	ret
+; (old Int19hMenu_RomBoot routine)
+;
+.romboot:		
+		int		BIOS_BOOT_FAILURE_INTERRUPT_18h	; Never returns		
