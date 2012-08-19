4475bfc1fa8acbfee02ecee6ced995ab2d86a15a
aitotat@gmail.com <aitotat@gmail.com@143130fb-dee8-9d2b-0d25-209ee59ef9c5>
2012-08-19 09:58:42 +0000

    Changes to XTIDE Universal BIOS:  * ATA-ID sector is now verified (P-CHS verification for ATA-4 and older + checksum for ATA-5 and later)

--- trunk/XTIDE_Universal_BIOS/Src/Initialization/AtaID.asm
+++ trunk/XTIDE_Universal_BIOS/Src/Initialization/AtaID.asm
@@ -21,6 +21,87 @@
 ; Section containing code
 SECTION .text
 
+;--------------------------------------------------------------------
+; AtaID_VerifyFromESSI
+;	Parameters:
+;		ES:SI:	Ptr to 512-byte ATA information read from the drive
+;	Returns:
+;		CF:		Set if failed to verify ATA-ID
+;				Cleared if ATA-ID verified succesfully
+;	Corrupts registers:
+;		AX, BX, CX
+;--------------------------------------------------------------------
+AtaID_VerifyFromESSI:
+	; We cannot start by reading ATA version since the ID might be
+	; corrupted. We start by making sure P-CHS values are valid.
+	; If they are, we assume the ATA ID to be valid. Fortunately we can do
+	; futher checking for ATA-5 and later since they contain signature and
+	; checksum bytes. Those are not available for ATA-4 and older.
+
+	; Verify P-CHS cylinders
+	mov		bx, ATA1.wCylCnt
+	mov		cx, MAX_VALID_PCHS_CYLINDERS
+	call	.CompareCHorSfromOffsetBXtoMaxValueInCX
+
+	add		bx, BYTE ATA1.wHeadCnt - ATA1.wCylCnt
+	mov		cl, MAX_VALID_PCHS_HEADS
+	call	.CompareCHorSfromOffsetBXtoMaxValueInCX
+
+	add		bx, BYTE ATA1.wSPT - ATA1.wHeadCnt
+	mov		cl, MAX_VALID_PCHS_SECTORS_PER_TRACK
+	call	.CompareCHorSfromOffsetBXtoMaxValueInCX
+
+	; We now verified P-CHS parameters so we assume ATA ID to be valid
+	; for ATA-4 and older. For ATA-5 and later we check signature
+	; and checksum.
+	mov		ax, [es:si+ATA6.wMajorVer]			; ATA-3 and later have this word
+	inc		ax
+	jz		SHORT .AtaIDverifiedSuccessfully	; FFFFh means no version info available
+	dec		ax
+	jz		SHORT .AtaIDverifiedSuccessfully	; Zero means no version info available
+	cmp		ax, A6_wMajorVer_ATA5
+	jb		SHORT .AtaIDverifiedSuccessfully	; ATA-3 and ATA-4 do not have checksum
+
+	; Check signature byte
+	cmp		BYTE [es:si+ATA6.bSignature], A6_wIntegrity_SIGNATURE
+	jne		SHORT .FailedToVerifyAtaID
+
+	; Check checksum byte
+	mov		cx, ATA6_size
+	call	Memory_SumCXbytesFromESSItoAL
+	test	al, al
+	jnz		SHORT .FailedToVerifyAtaID
+
+	; ATA-ID is now verified to be valid
+.AtaIDverifiedSuccessfully:
+	clc
+	ret
+
+;--------------------------------------------------------------------
+; .CompareCHorSfromOffsetBXtoMaxValueInCX
+;	Parameters:
+;		BX:		C, H or S offset to ATA-ID
+;		CX:		Maximum valid C, H or S value
+;		ES:SI:	Ptr to 512-byte ATA information read from the drive
+;	Returns:
+;		Exits from AtaID_VerifyFromESSI with CF set if invalid value
+;	Corrupts registers:
+;		AX
+;--------------------------------------------------------------------
+.CompareCHorSfromOffsetBXtoMaxValueInCX:
+	mov		ax, [es:bx+si]
+	test	ax, ax
+	jz		SHORT .InvalidPCHorSinOffsetBX
+	cmp		ax, cx			; Compare to max valid value
+	jbe		SHORT .ValidPCHorSinOffsetBX
+.InvalidPCHorSinOffsetBX:
+	add		sp, 2			; Clear return address for this function
+.FailedToVerifyAtaID:
+	stc						; Set carry to indicate invalid ATA-ID
+.ValidPCHorSinOffsetBX:
+	ret
+
+
 %ifdef MODULE_ADVANCED_ATA
 ;--------------------------------------------------------------------
 ; AtaID_GetMaxPioModeToAXandMinCycleTimeToCX
