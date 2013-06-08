88514cbad86d2a7114b74f666ce94cd4ad8b9f72
aitotat <aitotat@gmail.com>
2013-06-08 09:32:10 +0000

    Changes to XTIDE Universal BIOS:   * Standard boot procedure (INT 19h) works again.

--- XTIDE_Universal_BIOS/Src/Handlers/Int13hBiosInit.asm
+++ XTIDE_Universal_BIOS/Src/Handlers/Int13hBiosInit.asm
@@ -27,13 +27,31 @@ TEMPORARY_VECTOR_FOR_SYSTEM_INT13h		EQU		21h	; MS-DOS
 Int13hBiosInit_Handler:
 	LOAD_BDA_SEGMENT_TO	ds, ax
 
-	; Restore system INT 13h handler that we backuped
-	les		ax, [TEMPORARY_VECTOR_FOR_SYSTEM_INT13h*4]
-	mov		[BIOS_DISK_INTERRUPT_13h*4], ax
-	mov		[BIOS_DISK_INTERRUPT_13h*4+2], es
-
 	; Now install our handler and call 19h since non-standard motherboard BIOS did not
 	; do that or our INT 19h hander was replaced by other BIOS.
 	mov		WORD [BIOS_BOOT_LOADER_INTERRUPT_19h*4], Int19h_BootLoaderHandler
 	mov		[BIOS_BOOT_LOADER_INTERRUPT_19h*4+2], cs
 	int		BIOS_BOOT_LOADER_INTERRUPT_19h	; Does not return
+
+
+;--------------------------------------------------------------------
+; Int13hBiosInit_RestoreSystemHandler
+;	Parameters:
+;		Nothing
+;	Returns:
+;		Nothing
+;	Corrupts registers:
+;		AX, DS, ES
+;--------------------------------------------------------------------
+Int13hBiosInit_RestoreSystemHandler:
+	LOAD_BDA_SEGMENT_TO	ds, ax
+
+	; Is our very late init handler in place
+	cmp		WORD [BIOS_DISK_INTERRUPT_13h*4], Int13hBiosInit_Handler
+	jne		SHORT .SystemHandlerAlreadyInPlace
+
+	les		ax, [TEMPORARY_VECTOR_FOR_SYSTEM_INT13h*4]
+	mov		[BIOS_DISK_INTERRUPT_13h*4], ax
+	mov		[BIOS_DISK_INTERRUPT_13h*4+2], es
+.SystemHandlerAlreadyInPlace:
+	ret
