5213278bb3e08e1cf2e08c9b2de14f910c98927d
aitotat@gmail.com <aitotat@gmail.com@143130fb-dee8-9d2b-0d25-209ee59ef9c5>
2013-07-30 13:08:23 +0000

    Changes to Configurator v2:   * Forgot to update version info. Configurator now works with r563 and later.

--- trunk/XTIDE_Universal_BIOS_Configurator_v2/Src/Strings.asm
+++ trunk/XTIDE_Universal_BIOS_Configurator_v2/Src/Strings.asm
@@ -22,7 +22,7 @@ SECTION .data
 
 ; Menu title
 g_szProgramTitle:					db	"Configuration and Flashing program for XTIDE Universal BIOS v2.0.0.",LF,CR,NULL
-g_szXtideUniversalBiosSignature:	db	"XTIDE203",NULL
+g_szXtideUniversalBiosSignature:	db	"XTIDE204",NULL
 g_szBiosIsNotLoaded:				db	"BIOS is not loaded!",NULL
 g_szEEPROM:							db	"EEPROM",NULL
 g_szSourceAndTypeSeparator:			db	" : ",NULL
